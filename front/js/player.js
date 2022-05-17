5bd3e13995a2fb0c1acc50a9c16fcb6a4386b9cd
FrancoisRGNT <francois.regnaut@student.junia.com>
2022-05-17 02:30:26 +0200

    Ajout partiel de la chambre

--- front/js/player.js
+++ front/js/player.js
@@ -1,8 +1,9 @@
-let socket = io()
-
 let inRoom = true
 let inComputer = false
 let inBiblio = false
+let inTV = false
+let inFrigo = false
+let inConsole = false
 let PC_is_lock = true
 
 let PC = document.querySelector('#PC')
@@ -16,14 +17,22 @@ FondPC.style.display = "none"
 
 let Room = document.querySelector('#Room')
 Room.style.display = "block"
+let Room2 = document.querySelector('#Room2')
+Room2.style.display = "none"
 
 let ClickBureau = document.querySelector('.BureauClick')
 let Ordi = document.querySelector('#Ordi')  
 let ClickPC = document.querySelector('.clickPC')
 let Biblio = document.querySelector('#Biblio')
 let ClickBiblio = document.querySelector('.BiblioClick')
+let TV = document.querySelector('#TV')
+let ClickTV = document.querySelector('.TVClick')
+let Frigo = document.querySelector('#Frigo')
+let ClickFrigo = document.querySelector('.FrigoClick')
+let Console = document.querySelector('#Console')
+let ClickConsole = document.querySelector('.ConsoleClick')
 
-let canvas = document.getElementById("myCanvas");
+let canvas = document.querySelector(".myCanvas");
 let ctx = canvas.getContext('2d');
 ctx.translate(0.5, 0.5);
   
@@ -37,14 +46,12 @@ canvas.height = sizeHeight;
 canvas.style.width = sizeWidth;
 canvas.style.height = sizeHeight;
 
-
-
 let DownPressed = false;
 let UpPressed = false;
 let LeftPressed = false;
 let RightPressed = false;
 
-let x = window.innerWidth/3;
+let x = window.innerWidth/2;
 let y = window.innerHeight/2;
 let ballRadius = window.innerHeight*0.03294892915;
 
@@ -101,37 +108,133 @@ function keyUpHandler(e) {
 function updatePersonnage() {
     if(inRoom == true){
 
-
+        
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
         
         if(DownPressed) {
             y += 5;
-            if((y >= window.innerHeight*0.8813838550245) || ((x >= window.innerWidth*0.6811023622) && ( y >= window.innerHeight*0.535))){
+            if((y >= window.innerHeight*0.8813838550245) || 
+              ((x >= window.innerWidth*0.6811023622) && ( y >= window.innerHeight*0.535)) ||
+              ((y >= window.innerHeight*0.6455172413793103) && (x >= window.innerWidth*0.291015625) && (x <= window.innerWidth*0.335)) ||
+              ((y >= window.innerHeight*0.84) && (x <= window.innerWidth*0.291015625)) ||
+              ((x <= window.innerWidth*0.26171875) && (y >= window.innerHeight*0.74)) ||
+              ((x <= window.innerWidth*0.16) && (y >= window.innerHeight*0.6524137931034483)) ||
+              ((y >= window.innerHeight*0.6) && (x <= window.innerWidth*0.4537760416) && (x >= window.innerWidth*0.374) && (y <= window.innerHeight*0.775)) ||
+              ((y <= window.innerHeight*0.635) && (x >= window.innerWidth*0.52) && (y >= window.innerHeight*0.4868965517241379) && (x <= window.innerWidth*0.671875)) ||
+              ((x >= window.innerWidth*0.543) && (y <= window.innerHeight*0.7558620689655172) && (x <= window.innerWidth*0.6355) && (y >= window.innerHeight*0.4868965517241379)) ||
+              ((x >= window.innerWidth*0.54) && (y >= window.innerHeight*0.8317241379310345) && (x <= window.innerWidth*0.649)) ||
+              ((x >= window.innerWidth*0.72) && (y >= window.innerHeight*0.4937931034482759)) ||
+              ((y >= window.innerHeight*0.3627586206896552) && (x >= window.innerWidth*0.245) && (y <= window.innerHeight*0.568) && (x <= window.innerWidth*0.3561197916))){
                 y -=5
             }
         }
         if(UpPressed) {
             y -= 5;
-            if((y <= window.innerHeight*0.29654036244 ) || ((x >= window.innerWidth*0.67) && (y <= window.innerHeight*0.38714991762))){
+            if((y <= window.innerHeight*0.29654036244 ) || 
+              ((x >= window.innerWidth*0.665) && (y <= window.innerHeight*0.38714991762)) ||
+              ((y >= window.innerHeight*0.6) && (x <= window.innerWidth*0.4537760416) && (x >= window.innerWidth*0.374) && (y <= window.innerHeight*0.775)) ||
+              ((y <= window.innerHeight*0.635) && (x >= window.innerWidth*0.52) && (y >= window.innerHeight*0.4868965517241379) && (x <= window.innerWidth*0.667)) ||
+              ((x >= window.innerWidth*0.543) && (y <= window.innerHeight*0.7558620689655172) && (x <= window.innerWidth*0.6355) && (y >= window.innerHeight*0.4868965517241379)) ||
+              ((x >= window.innerWidth*0.438) && (y <= window.innerHeight*0.34)) ||
+              ((y <= window.innerHeight*0.3351724137931034) && (x <= window.innerWidth*0.3235677083)) ||
+              ((x <= window.innerWidth*0.195) && (y <= window.innerHeight*0.385)) ||
+              ((x <= window.innerWidth*0.3561197916) && (y <= window.innerHeight*0.568) && (x >= window.innerWidth*0.29105)) ||
+              ((x >= window.innerWidth*0.796) && (y <= window.innerHeight*0.41)) ||
+              ((y >= window.innerHeight*0.3627586206896552) && (x >= window.innerWidth*0.245) && (y <= window.innerHeight*0.568) && (x <= window.innerWidth*0.3561197916))){
                 y +=5
             }
         }
         if(LeftPressed) {
             x -= 5;
-            if(x <= window.innerWidth*0.13779527559){
+            if((x <= window.innerWidth*0.125) ||
+              ((x <= window.innerWidth*0.335) && (y >= window.innerHeight*0.6455172413793103) && (x >= window.innerWidth*0.291015625)) ||
+              ((x <= window.innerWidth*0.26171875) && (y >= window.innerHeight*0.74)) ||
+              ((x <= window.innerWidth*0.16) && (y >= window.innerHeight*0.6524137931034483)) ||
+              ((y >= window.innerHeight*0.6) && (x <= window.innerWidth*0.4537760416) && (x >= window.innerWidth*0.374) && (y <= window.innerHeight*0.775)) ||
+              ((y <= window.innerHeight*0.635) && (x >= window.innerWidth*0.52) && (y >= window.innerHeight*0.4868965517241379) && (x <= window.innerWidth*0.667)) ||
+              ((x >= window.innerWidth*0.543) && (y <= window.innerHeight*0.7558620689655172) && (x <= window.innerWidth*0.6355) && (y >= window.innerHeight*0.4868965517241379)) ||
+              ((x >= window.innerWidth*0.54) && (y >= window.innerHeight*0.8317241379310345) && (x <= window.innerWidth*0.649)) ||
+              ((x <= window.innerWidth*0.195) && (y <= window.innerHeight*0.385)) ||
+              ((x <= window.innerWidth*0.3561197916) && (y <= window.innerHeight*0.568) && (x >= window.innerWidth*0.29105))) {
                 x += 5;
             }
         }
         if(RightPressed) {
             x += 5;
-            if(((x >= window.innerWidth*0.6775) && ( y >= window.innerHeight*0.535)) || ( x>= window.innerWidth*0.875) || ((y<=window.innerHeight*0.38714991762) && (x >=window.innerWidth*0.67))){
+            if(((x >= window.innerWidth*0.665) && ( y >= window.innerHeight*0.535)) || 
+              (x >= window.innerWidth*0.875) || 
+              ((y <= window.innerHeight*0.38714991762) && (x >= window.innerWidth*0.67)) ||
+              ((y >= window.innerHeight*0.6455172413793103) && (x >= window.innerWidth*0.291015625) && (x <= window.innerWidth*0.335)) ||
+              ((y >= window.innerHeight*0.6) && (x <= window.innerWidth*0.4537760416) && (x >= window.innerWidth*0.374) && (y <= window.innerHeight*0.775)) ||
+              ((y <= window.innerHeight*0.635) && (x >= window.innerWidth*0.52) && (y >= window.innerHeight*0.4868965517241379) && (x <= window.innerWidth*0.667)) ||
+              ((x >= window.innerWidth*0.543) && (y <= window.innerHeight*0.7558620689655172) && (x <= window.innerWidth*0.6355) && (y >= window.innerHeight*0.4868965517241379)) ||
+              ((x >= window.innerWidth*0.54) && (y >= window.innerHeight*0.8317241379310345) && (x <= window.innerWidth*0.649)) ||
+              ((x >= window.innerWidth*0.438) && (y <= window.innerHeight*0.34)) ||
+              ((x <= window.innerWidth*0.3561197916) && (y <= window.innerHeight*0.568) && (x >= window.innerWidth*0.29105)) ||
+              ((x >= window.innerWidth*0.828125)) ||
+              ((x >= window.innerWidth*0.796) && (y <= window.innerHeight*0.41)) ||
+              ((x >= window.innerWidth*0.72) && (y >= window.innerHeight*0.4937931034482759)) ||
+              ((y >= window.innerHeight*0.3627586206896552) && (x >= window.innerWidth*0.245) && (y <= window.innerHeight*0.568) && (x <= window.innerWidth*0.3561197916))) {
+
                 x -= 5;
             }
         }
 
+        //Test si je suis derrière le canapé
+        if((x >= window.innerWidth*0.5227864583333333) && (y >= window.innerHeight*0.4524137931034483) && (x <= window.innerWidth*0.6627604166666667) && (y <= window.innerHeight*0.583448275862069)){
+            Room.style.display = "none"
+            Room2.style.display = "block"
+        }
+        else{
+            Room2.style.display = "none"
+            Room.style.display = "block"
+        }
+
+        //Test si je suis derrière la plante
+        if((x >= window.innerWidth*0.2428385416666667) && (y <= window.innerHeight*0.376551724137931) && (x <= window.innerWidth*0.30)){
+            Room.classList.remove('Room');
+            Room.classList.add('Room3')
+        }
+        else{
+            Room.classList.remove('Room3');
+            Room.classList.add('Room')
+        }
+
+        //Test si je suis derrière la télé
+        if ((x >= window.innerWidth * 0.548828125) && (y >= window.innerHeight * 0.7972413793103448) && (x <= window.innerWidth * 0.6497395833333333)) {
+
+            Room.classList.remove('Room');
+            Room.classList.add('Room4')
+        }
+        else {
+            Room.classList.remove('Room4');
+            Room.classList.add('Room')
+        }
+
+        //Test si je suis derrière la table de chevet
+        if((x <= window.innerWidth*0.1549479166666667) && (y >= window.innerHeight*0.6248275862068966)){
+
+            Room.classList.remove('Room');
+            Room.classList.add('Room5')
+        }
+        else {
+            Room.classList.remove('Room5');
+            Room.classList.add('Room')
+        }
+
+        //Test si je suis derrière la console
+        if ((x >= window.innerWidth * 0.7213541666666667) && (y >= window.innerHeight * 0.485)){ 
+        Room.classList.remove('Room');
+        Room.classList.add('Room6')
+        }
+        else {
+        Room.classList.remove('Room6');
+        Room.classList.add('Room')
+        }
+
         //Test si je suis devant l'odinateur
-        if((x>=window.innerWidth*0.30314960629) && (x<=window.innerWidth*0.37401574803) && (y<=window.innerHeight*0.36243822075)){
+        if((x>=window.innerWidth*0.195) && (x<=window.innerWidth*0.22) && (y<=window.innerHeight*0.38)){
             inComputer = true
         }
         else{
@@ -140,30 +243,76 @@ function updatePersonnage() {
         
         //Affiche la description de l'ordi
         if(inComputer){
-            Ordi.style.backgroundColor = "red"
             ClickBureau.style.display = "block"
         }
         else{
-            Ordi.style.backgroundColor = "gray"
             ClickBureau.style.display = "none"
         }
 
-        //Test si je suis devant la bibliothèque
-        if((y >= window.innerHeight*0.52718286655) && (y <= window.innerHeight*0.62602965403) && (x <=window.innerWidth*0.1653543307)){
+        //Test si je suis devant la table
+        if((x >= window.innerWidth*0.32421875) && (y <= window.innerHeight*0.5144827586206897) && (x <=window.innerWidth*0.373046875) && (y >= window.innerHeight*0.4110344827586207)){
             inBiblio = true
         }
         else{
             inBiblio = false
         }
 
+        //Affiche la description de la table
         if(inBiblio){
-            Biblio.style.backgroundColor = "red"
             ClickBiblio.style.display = "block"
         }
         else{
-            Biblio.style.backgroundColor = "gray"
             ClickBiblio.style.display = "none"
         }
+
+        //Test si je suis devant la TV
+        if((x >= window.innerWidth*0.545) && (y >= window.innerHeight*0.8041379310344828) && (x <= window.innerWidth*0.6432291666666667)){
+            inTV = true
+        }
+        else{
+            inTV = false
+        }
+
+         //Affiche la description de la TV
+         if(inTV){
+            ClickTV.style.display = "block"
+        }
+        else{
+            ClickTV.style.display = "none"
+        }
+
+
+        //Test si je suis devant le Frigo
+        if ((x >= window.innerWidth * 0.63) && (y <= window.innerHeight * 0.3627586206896552)) {
+            inFrigo = true
+        }
+        else {
+            inFrigo = false
+        }
+
+        //Affiche la description du Frigo
+        if (inFrigo) {
+            ClickFrigo.style.display = "block"
+        }
+        else {
+            ClickFrigo.style.display = "none"
+        }
+
+         //Test si je suis devant la Console
+         if ((x >= window.innerWidth*0.7506510416666667) && (y >= window.innerHeight*0.4662068965517241)) {
+            inConsole = true
+        }
+        else {
+            inConsole = false
+        }
+
+        //Affiche la description de la Console
+        if (inConsole) {
+            ClickConsole.style.display = "block"
+        }
+        else {
+            ClickConsole.style.display = "none"
+        } 
     }   
 }
 
@@ -214,7 +363,6 @@ $(document).ready(function() {
             PClock.style.display = "none"
             PC.style.display = "block"
             PC_is_lock = false
-            socket.emit('whichMission',(2))
 		}
 	}
 })
