c3280d7c320cf7a96fb1e463b46dfb37d5def155
EliotLeleu <eliot.leleu@student.yncrea.fr>
2022-05-18 13:17:30 +0200

    rien

--- front/js/page.js
+++ front/js/page.js
@@ -285,7 +285,7 @@ $(document).ready(function() {
     
     //Parametrage InstaGroove 
 
-    $('.iconereg').click(function (){
+    $('.iconeregInsta').click(function (){
         InstaGroovescreenOpen.style.display = "none"
         InstaGroovescreen_Para.style.display = "block"
     })
@@ -314,12 +314,7 @@ $(document).ready(function() {
         InstaGroovescreen_mess.style.display = "none"
         InstaGroovescreenOpen.style.display = "block"
     }) 
-
-    $('.annulerMdpImail').click(function (){
-        Imailscreen_changeMdp.style.display = "none"
-        ImailscreenOpen.style.display = "block"
-    })
-
+    
     //Fonction pour changer de page
     $('.defile').click(function(){
         //va = div que tu veux masquer
